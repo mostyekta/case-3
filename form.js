891b9e022f74c1e0b80dcc6206143840485d2011
mostyekta <mostyekta@hotmail.com>
2023-12-28 17:23:35 +0100

    Ändrat style , fixat remove button

--- form.js
+++ form.js
@@ -1,96 +1,80 @@
 const form = document.querySelector("form");
 const inputText = document.querySelector("input#text");
-const ul = document.querySelector("ul"); 
-const li= document.querySelector("li");
-const button = document.querySelector("btn-remove");
+const ul = document.querySelector("ul");
+const div = document.querySelector("div");
+
+const button = document.querySelector("btnRemove");
+
+
+const url = document.createElement("URL");
+
+form.addEventListener("submit", function (event) {
+
+  event.preventDefault();
+  const textValue = inputText.value;
+
+  const li = document.createElement("LI");
+  li.appendChild(document.createTextNode(textValue));
+  li.classList.add("liAncer");
+
+  const a = document.createElement("a");
+  const linkText = document.getElementById("link");
+  a.innerText = "https://" + linkText.value;
+  a.href = linkText;
+  a.target = "_blank";
+  a.classList.add("aLink");
+
+  a.appendChild(url);
+  li.appendChild(a);
+  ul.appendChild(li);
 
 
 
-
-form.addEventListener("submit", function(event){
-
-    event.preventDefault();
-    const textValue= inputText.value;
-
-    const li = document.createElement("li");
-    li.appendChild(document.createTextNode(textValue));
-    ul.appendChild(li);
-//    ul.insertBefore(li, ul.firstElementChild);
+  const btnRemove = document.createElement("button");
+  btnRemove.innerText = "Remove";
+  li.appendChild(btnRemove);
+  btnRemove.classList.add("buttonRemove");
 
 
-const btnRemove = document.createElement("button");
- btnRemove.textContent="Remove";
-li.appendChild(btnRemove);
 
-
-  btnRemove.style.background="#bb1111";
-  btnRemove.style.width="80px";
-  btnRemove.style.height="40px";
-  btnRemove.style.color="white";
-  btnRemove.style.marginLeft="100px";
-
-
- ul.addEventListener("click",(event) => {
-
- 
-  if( event.target.nodeName === "BUTTON"){
-   const button = event.target;
-  
-  if(button.textContent === "Remove"){
-
-      ul.remove(li);
-    
-   }
- }
-
-
-});
-
+  btnRemove.addEventListener("click", function () {
+    li.remove();
+  });
 
 
   inputText.value = "";
+  linkText.value = "";
 
 
+  const radios = document.querySelectorAll("input[type=radio]");
 
-const radios = document.querySelectorAll("input[type=radio]");
+  let place;
 
-let place;
+  radios.forEach(radio => {
 
-radios.forEach (radio  => {
+    if (radio.checked) {
 
-  if (radio.checked){
-
-    place = radio.value;
-  }
-
-
-  if(place === "first"){
-
-    ul.insertBefore(li, ul.firstElementChild);
-   }
-   else{
-    ul.appendChild(li);
-   }
-  
-
-});
- 
-});
-
-ul.addEventListener("click",(event)=> {
-  
-    event.target.classList.toggle("backgrund");
-   });
-  
-  
-   ul.addEventListener("contextmenu",(event)=> {
-     event.preventDefault();
-  
-    if( event.target.nodeName === "LI"){
-  
-      event.target.remove();
+      place = radio.value;
     }
-  
-   });
-  
-   
+
+
+    if (place === "first") {
+
+      ul.insertBefore(li, ul.firstElementChild);
+    }
+    else {
+      ul.appendChild(li);
+    }
+
+
+  });
+
+});
+
+ul.addEventListener("click", (event) => {
+
+  event.target.classList.toggle("backgrund");
+});
+
+
+
